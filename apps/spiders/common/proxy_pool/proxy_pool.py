a3c1ec1ba5d0eb7514056162dcb7d8cd4c00dc06
mingz <305603665@qq.com>
2016-12-09 18:38:55 +0800

    init

--- apps/spiders/common/proxy_pool/proxy_pool.py
+++ apps/spiders/common/proxy_pool/proxy_pool.py
@@ -1,10 +1,14 @@
 # -*- coding:utf-8 -*-
 __author__ = 'zhaojm'
 
+from ssh_config import username, password, ssh_ip_list
 
 
 class ProxyPool(object):
     def __init__(self):
+        self._username = username
+        self._password = password
+        self._sss_config = ssh_ip_list
         pass
 
     def request_one(self):
