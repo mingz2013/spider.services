bd5548ff68aa6cf4297b73306655d24227b9226e
mingz <305603665@qq.com>
2016-12-12 17:58:02 +0800

    init

--- apps/website/controllers/proxy_controller.py
+++ apps/website/controllers/proxy_controller.py
@@ -11,5 +11,5 @@ api = Blueprint('proxy_controller', __name__, url_prefix='/proxy')
 
 @api.route('/test_ip', methods=['GET'])
 def test_ip():
-    current_app.logging.info("remote_addr: %s" % request.remote_addr)
+    current_app.logger.info("remote_addr: %s" % request.remote_addr)
     return request.remote_addr
