c4b93bed1f550ba83c495f13ea13e288c9acea8e
mingz <305603665@qq.com>
2016-12-12 15:47:18 +0800

    init

--- apps/spiders/common/proxy_pool/proxy.py
+++ apps/spiders/common/proxy_pool/proxy.py
@@ -3,7 +3,9 @@ __author__ = 'zhaojm'
 
 
 class Proxy(object):
-    def __init__(self):
+    def __init__(self, ip, port):
+        self._ip = ip
+        self._port = port
         pass
 
     def refresh_ip(self):
--- apps/spiders/common/proxy_pool/proxy_pool.py
+++ apps/spiders/common/proxy_pool/proxy_pool.py
@@ -1,18 +1,36 @@
 # -*- coding:utf-8 -*-
 __author__ = 'zhaojm'
 
+import random
+
 from ssh_config import username, password, ssh_ip_list
+from proxy import Proxy
 
 
 class ProxyPool(object):
     def __init__(self):
         self._username = username
         self._password = password
-        self._sss_config = ssh_ip_list
+        self._proxy_list = []
+        self._lock_list = []
+        self._parse_ssh_list(ssh_ip_list)
         pass
+
+    def _parse_ssh_list(self, ssh_list):
+        for item in ssh_list:
+            p = Proxy(item['ip'], item['port'])
+            self._proxy_list.append(p)
 
     def request_one(self):
-        pass
+        if len(self._proxy_list) > 0:
+            proxy = self._proxy_list[random.randint(len(self._proxy_list))]
+            self._proxy_list.remove(proxy)
+            self._lock_list.append(proxy)
+            return proxy
+        else:
+            return None
 
-    def release_one(self):
+    def release_one(self, proxy):
+        self._proxy_list.append(proxy)
+        self._lock_list.remove(proxy)
         pass
