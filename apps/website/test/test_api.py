10d245b5cb4f7540a9f7d2af29d0385940951012
mingz <305603665@qq.com>
2016-12-14 11:30:59 +0800

    init

---
+++ apps/website/test/test_api.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+import requests
+import logging
+
+
+def test_api_push():
+    url = "http://localhost/push"
+    data = {"company_name": "百度"}
+    response = requests.post(url, data)
+    logging.info(response)
+    logging.info(response.content)
+    pass
