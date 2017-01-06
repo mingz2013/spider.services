4494dd4ee141096b63f725ca3a075977b8e3f8a9
mingz <305603665@qq.com>
2017-01-06 11:41:14 +0800

    init

---
+++ apps/spiders/common/proxy_server_pool/ssh_config.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+username = 'root'
+password = 'aa0d0a4a9166'
+ssh_ip_list = [
+    {"ip": '118.119.102.36', "port": 20363},
+    {"ip": '113.59.34.55', "port": 20119},
+    {"ip": '221.10.205.22', "port": 20611},
+    {"ip": '221.10.101.119', "port": 20461},
+    {"ip": '221.10.170.70', "port": 20381},
+    {"ip": '221.10.137.9', "port": 20181}
+]
+
+proxy_port = 5839
+
+proxy_ip_url = "http://123.206.6.251/proxy/test_ip"
